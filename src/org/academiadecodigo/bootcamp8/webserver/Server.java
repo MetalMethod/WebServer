d6f90d5be4ee685709415d85a6ff4e68525d9c30
Igor Busquets <igorbusquets@gmail.com>
2017-06-20 20:37:33 +0100

    Added host adress and name resolution on startup

--- src/org/academiadecodigo/bootcamp8/webserver/Server.java
+++ src/org/academiadecodigo/bootcamp8/webserver/Server.java
@@ -5,8 +5,8 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+//import java.util.logging.Level;
+//import java.util.logging.Logger;
 
 /**
  * This class implements a multithreaded web server.
@@ -20,7 +20,7 @@ public class Server {
 
     public static final int PORT = 9090;
 
-    private static final Logger logger = Logger.getLogger(Server.class.getName());
+    //private static final Logger logger = Logger.getLogger(Server.class.getName());
 
     public static void main(String[] args) {
 
@@ -32,15 +32,18 @@ public class Server {
             System.out.println("##### Done @ AcademiaDeCodigo.org - bootcamp8 - june/2017 #####");
             System.out.println("#####      written by Igor Busquets - version 0.0.4       #####");
             System.out.println("###############################################################");
+            System.out.println("#####      host name: "  + serverSocket.getInetAddress().getHostName()
+                                 + "   host adress: " + serverSocket.getInetAddress().getHostAddress() + "      #####");
             System.out.println("#####         Waiting for connections at port "
                     + serverSocket.getLocalPort()+"        #####");
             System.out.println("###############################################################\n");
+
 
             while (true) {
 
                 //init new socket for every request
                 Socket clientSocket = serverSocket.accept();
-                logger.log(Level.INFO, "new connection from " + clientSocket.getInetAddress());
+                //logger.log(Level.INFO, "new connection from " + clientSocket.getInetAddress());
 
 
                 //strong thread pool creation
